87dc89389e60ff67f65c3392d6d7eb46fc74ab46
Kawser Hossain <irumorgan420@gmail.com>
2023-02-26 23:14:46 +0600

    all image fix

--- components/hero/hero.js
+++ components/hero/hero.js
@@ -1,3 +1,5 @@
+import Image from "next/image";
+
 const Hero = () => {
   return (
     <>
@@ -8,7 +10,7 @@ const Hero = () => {
            */}
           <div className="sm:order-2">
             <p className="font-Roboto text-[#FF4F6E] text-[24px]">
-              Hello, I'm Nancy
+              {`Hello, I'm Nancy`}
             </p>
             <p className="font-Ubuntu text-[#031E1D] text-[36px] font-bold tracking-[-0.03em] leading-[45px] w-[300px] sm:w-[300px] sm:h-[180px] sm:text-[40px]">
               Professional UI/UX Designer & Web Developer
@@ -19,9 +21,11 @@ const Hero = () => {
                Hire Button 
               */}
               <div className="hidden sm:flex max-w-[150px] border-b-[3px]    pb-3 pt-6 drop-shadow-sm items-center gap-4">
-                <img
+                <Image
                   className="w-[36px]"
-                  src="img/hero-hire-arrow.png"
+                  height={100}
+                  width={100}
+                  src={`/img/hero-hire-arrow.png`}
                   alt="img/hero-hire-arrow"
                 />
                 <p className="text-[#343E42] font-Poppins font-bold text-[12px]">
@@ -29,9 +33,11 @@ const Hero = () => {
                 </p>
                 {/* arrow Yellow */}
                 <div className="absolute top-[-60px] right-[-158px] rotate-[-20deg] hidden min-[730px]:flex">
-                  <img
+                  <Image
+                    height={100}
+                    width={100}
                     className="max-w-[160px]"
-                    src="img/hero-hire-arrow-yellow.png"
+                    src="/img/hero-hire-arrow-yellow.png"
                     alt="img/hero-hire-arrow-yellow"
                   />
                 </div>
@@ -40,16 +46,26 @@ const Hero = () => {
 
             {/* Social Icon */}
             <div className="pt-4 hidden sm:flex">
-              <img className="w-[25px]" src="/img/hero-social-fb.png" alt="" />
-              <img
+              <Image
+                height={100}
+                width={100}
+                className="w-[25px]"
+                src="/img/hero-social-fb.png"
+                alt="hero-social-fb"
+              />
+              <Image
+                height={100}
+                width={100}
                 className="w-[25px]"
                 src="/img/hero-social-basket.png"
-                alt=""
+                alt="hero-social-basket"
               />
-              <img
+              <Image
+                height={1000}
+                width={1000}
                 className="w-[25px]"
                 src="/img/hero-social-twitter.png"
-                alt=""
+                alt="hero-social-twitter"
               />
             </div>
           </div>
@@ -61,22 +77,31 @@ const Hero = () => {
             {/* 
               Background Image Reative
            */}
-            <img
+            <Image
+              height={1000}
+              width={1000}
               className="w-[290px] sm:max-w-[600px] md:w-800px"
               src="/img/hero-girl-smile-img-bg.png"
               alt="hero-girl-smile-img-bg"
             />
 
             <div className="absolute bottom-0">
-              <img
+              <Image
+                height="1000"
+                width="1000"
                 className="w-[250px] sm:max-w-[600px] md:w-800px"
-                src="/img/hero-girl-smile-img.png"
+                src={`/img/hero-girl-smile-img.png`}
                 alt="hero-girl-smile-img"
               />
 
               {/* Image Dot Dot */}
               <div className="absolute max-w-[55px] bottom-0 opacity-60">
-                <img src="/img/hero-girl-smile-dot.png" alt="" />
+                <Image
+                  height="1000"
+                  width="1000"
+                  src="/img/hero-girl-smile-dot.png"
+                  alt="hero-girl-smile-dot"
+                />
               </div>
             </div>
           </div>
@@ -86,17 +111,32 @@ const Hero = () => {
            Hire Button 
           */}
         <div className="max-w-[280px] border-b-[3px] pb-3 pt-4 drop-shadow-sm flex items-center gap-7 sm:hidden">
-          <img src="img/hero-hire-arrow.png" alt="" />
+          <Image
+            height={1000}
+            width={1000}
+            src="/img/hero-hire-arrow.png"
+            alt="hero-hire-arrow"
+          />
           <p className="text-[#343E42] font-Poppins font-bold text-[18px]">
             Hire Me Now
           </p>
         </div>
 
         <div className="absolute bottom-0 left-0 hidden sm:flex">
-          <img src="img/hero-bottom-red.png" alt="" />
+          <Image
+            height={1000}
+            width={1000}
+            src="/img/hero-bottom-red.png"
+            alt="hero-bottom-red"
+          />
         </div>
         <div className="absolute bottom-0 right-0 hidden sm:flex">
-          <img src="img/hero-right-green.png" alt="" />
+          <Image
+            height={1000}
+            width={1000}
+            src="/img/hero-right-green.png"
+            alt="hero-right-green"
+          />
         </div>
       </div>
     </>
